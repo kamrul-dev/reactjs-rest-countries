72bcf3f891ae91197ee50f64fb7de6cc8f4e956e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-17 09:02:50 +0600

    Footer component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Countries from './components/Countries/Countries';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Countries></Countries>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='footer-text'>All right reserved Kamrul Hasan</p>
+        </div>
+    );
+};
+
+export default Footer;
